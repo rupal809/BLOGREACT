d17ba19f08ea4268f3b303530254ee38cfd23b68
rupal809 <rupalagarwal2005@gmail.com>
2026-06-17 14:26:30 +0530

    INITIAL BUILD

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      Tecjhnology BLOG
+     <ul>
+  <li>React Basics</li>
+  <li>Introduction to JavaScript</li>
+  <li>HTML </li>
+  <li>CSS </li>
+  <li>Understanding JSX</li>
+  <li>React Components</li>
+  <li>Props in React</li>
+  <li>State </li>
+  <li>React </li>
+  <li>Bootstrap </li>
+</ul> 
+    </div>
+  )
+}
+
+export default App
